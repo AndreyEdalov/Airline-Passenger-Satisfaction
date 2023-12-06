724f6a95f72e2cda42ad9d5b9cf7c0712619f094
Desperado <86427424+AndreyEdalov@users.noreply.github.com>
2023-12-06 20:18:10 +0300

    update

--- streamlit.py
+++ streamlit.py
@@ -3,20 +3,19 @@ import streamlit as st
 import seaborn as sns 
 import matplotlib.pyplot as plt
 
-# Загрузка данных
-st.title('Airline Passenger Satisfaction')
 df = pd.read_csv("train.csv")
+df["Total score"] = df[["Inflight wifi service","Departure/Arrival time convenient","Ease of Online booking","Gate location","Food and drink","Online boarding","Seat comfort","Inflight entertainment","On-board service","Leg room service","Baggage handling","Inflight service","Cleanliness","Checkin service"]].sum(axis=1)
 
-#Age distribution
-num_bins = st.slider('Select number of bins:', min_value=25, max_value=75, value=20)
-sns.set(style="whitegrid")
+st.title('Airline Passenger Satisfaction')
+
+#Histogram
+selected_feature = st.selectbox('Select Feature:', ['Age', 'Flight Distance', "Total score"])
+
+num_bins = st.slider('Select number of bins:', min_value=25, max_value=75, value=55)
 fig, ax = plt.subplots(figsize=(8, 6))
-sns.histplot(df["Age"], kde=True, bins=num_bins, edgecolor="black", color="skyblue")
+sns.histplot(df[selected_feature], kde=True, bins=num_bins, edgecolor="black", color="skyblue")
 ax.grid(axis="y", linestyle="--", alpha=0.7)
-ax.set_xlabel('Age', fontsize=12)
+ax.set_xlabel(selected_feature, fontsize=12)
 ax.set_ylabel('Number of People', fontsize=12)
-ax.set_title('Age Distribution', fontsize=16)
-ax.legend(labels=['Age Distribution'], loc='upper right')
-info_box = f"Mean Age: {df['Age'].mean():.2f}\nMedian Age: {df['Age'].median()}\nMax Age: {df['Age'].max()}"
-st.sidebar.text(info_box)
+ax.set_title(f'{selected_feature} Distribution', fontsize=16)
 st.pyplot(fig)
