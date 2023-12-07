6eaf0522d32528000bb28fd7cc3aab813e2135bb
Desperado <86427424+AndreyEdalov@users.noreply.github.com>
2023-12-07 16:05:26 +0300

    update

--- streamlit.py
+++ streamlit.py
@@ -13,57 +13,72 @@ df.loc[df["Customer Type"] == "disloyal Customer", "Customer Type"] = "Disloyal 
 #Making main article
 st.title('Airline Passenger Satisfaction')
 
-#Histograms
-
-selected_feature = st.selectbox('Select Feature:', ['Age', 'Flight Distance', "Total score","Class","Customer Type"])
-age = dict(df[selected_feature].value_counts().sort_values())
-fig = go.Figure([go.Bar(x=list(age.keys()), y=list(age.values()))])
-fig.update_layout(
-    xaxis_title=selected_feature, 
-    yaxis_title='Number of people',  
-)
-st.plotly_chart(fig)
-
-st.markdown("---")
+#Sidebar
+sidebar = st.sidebar
+selected_option = st.sidebar.selectbox('Select Visualization', ['Histograms', 'Age Distribution', 'Correlation', 'Pie Charts'])
 
 
-fig = px.histogram(df, x="Age", color="satisfaction",
-                   color_discrete_sequence=px.colors.qualitative.Antique)
-fig.update_layout(
-    title="<b>Age Distribution by Satisfaction</b>",
-    barmode="overlay"
-)
-fig.update_layout(
-    xaxis_title="Age", 
-    yaxis_title='Number of people',  
-)
-fig.update_traces(opacity=0.6)
-st.plotly_chart(fig)
+def show_fig1():
+    selected_feature = st.selectbox('Select Feature:', ['Age', 'Flight Distance', "Total score","Class","Customer Type"])
+    age = dict(df[selected_feature].value_counts().sort_values())
+    fig = go.Figure([go.Bar(x=list(age.keys()), y=list(age.values()))])
+    fig.update_layout(
+        xaxis_title=selected_feature, 
+        yaxis_title='Number of people',  
+    )
+    st.plotly_chart(fig)
+    
+def show_fig2():
+    fig2 = px.histogram(df, x="Age", color="satisfaction",
+                        color_discrete_sequence=px.colors.qualitative.Antique)
+    fig2.update_layout(
+        title="<b>Age Distribution by Satisfaction</b>",
+        barmode="overlay"
+    )
+    fig2.update_layout(
+        xaxis_title="Age", 
+        yaxis_title='Number of people',  
+    )
+    fig2.update_traces(opacity=0.6)
+    st.plotly_chart(fig2)
+    
+def show_fig3():
+    fig3 = px.scatter(
+        df,
+        x="Flight Distance",
+        y="Arrival Delay in Minutes",
+        marginal_x="histogram",
+        marginal_y="histogram",
+        title="<b>Correlation between Flight Distance and Arrival Delay",
+        color_discrete_sequence=px.colors.qualitative.Antique
+    )
+    st.plotly_chart(fig3)
+def show_fig4():
+    selected_feature = st.selectbox('Select Feature:', ["Gender", "Type of Travel","satisfaction"])
+    gender_info = df[selected_feature].value_counts()
+    pie_fig = go.Figure(data=[go.Pie(labels=gender_info.index, values=gender_info.values)])
 
-st.markdown("---")
+    pie_fig.update_layout(
+        title=selected_feature+" Distribution",
+        barmode="overlay"
+    )
+    st.plotly_chart(pie_fig)
+    
+    
 
-#Scatter
-fig = px.scatter(
-    df,
-    x="Flight Distance",
-    y="Arrival Delay in Minutes",
-    marginal_x="histogram",
-    marginal_y="histogram",
-    title="<b>Correlation between Flight Distance and Arrival Delay",
-    color_discrete_sequence=px.colors.qualitative.Antique
-)
-st.plotly_chart(fig)
+if selected_option == "Histograms":
+    show_fig1()
+elif selected_option == "Age Distribution":
+    show_fig2()
+elif selected_option == "Correlation":
+    show_fig3()
+elif selected_option == "Pie Charts":
+    show_fig4()
+else:
+    st.write("Please choose a table from the sidebar.")
 
-st.markdown("---")
+    
+    
+    
 
-#Pie
-selected_feature = st.selectbox('Select Feature:', ["Gender", "Type of Travel","satisfaction"])
-gender_info = df[selected_feature].value_counts()
-pie_fig = go.Figure(data=[go.Pie(labels=gender_info.index, values=gender_info.values)])
-
-pie_fig.update_layout(
-    title=selected_feature+" Distribution",
-    barmode="overlay"
-)
-
-st.plotly_chart(pie_fig)
+    
